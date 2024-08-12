589c8411b328b5aad64092d2f85c678a74a5db5d
Om Mandhane <stillwake27@gmail.com>
2024-08-12 14:04:38 +0530

    odom added to simple controller

--- install/rplidar_ros/share/rplidar_ros/launch/view_rplidar.launch.py
+++ install/rplidar_ros/share/rplidar_ros/launch/view_rplidar.launch.py
@@ -1,1 +1,17 @@
-/home/om/slambot/src/rplidar_ros2/launch/view_rplidar.launch.py
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/rplidar.launch.py'])),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', [ThisLaunchFileDir(), '/../rviz/rplidar.rviz']],
+        )
+    ])
---
+++ src/new_robot_firmware/include/new_robot_firmware/new_robot_interface.hpp
@@ -0,0 +1,18 @@
+#ifndef NEW_ROBOT_INTERFACE_HPP
+#define NEW_ROBOT_INTERFACE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <hardware_interrface/system_interface.hpp>
+
+namespace newrobot_interface{
+
+class NewRobotInterface :public hardware_interface::SystemInterface
+{
+public:
+
+
+};
+
+}
+
+#endif
