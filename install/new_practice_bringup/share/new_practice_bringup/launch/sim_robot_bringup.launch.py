545630dd5146674b785a8c5ab0e841933aa476f8
Om Mandhane <stillwake27@gmail.com>
2024-09-23 09:20:58 +0530

    new bringup launch completed

--- install/new_practice_bringup/share/new_practice_bringup/launch/sim_robot_bringup.launch.py
+++ install/new_practice_bringup/share/new_practice_bringup/launch/sim_robot_bringup.launch.py
@@ -1,44 +1,33 @@
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 import os
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.conditions import UnlessCondition, IfCondition
 
 
 def generate_launch_description():
-    joystick = DeclareLaunchArgument("joystick",
-    default_value="True")
-
-    use_joystick = LaunchConfiguration("joystick")
-
+    # Include the Gazebo launch file
     gazebo_sim = IncludeLaunchDescription(
         os.path.join(get_package_share_directory("new_robot_description"),
-        "launch",
-        "gazebo.launch.py")
+                     "launch",
+                     "gazebo.launch.py")
     )
 
-    controller =  IncludeLaunchDescription(
-    os.path.join(
-        get_package_share_directory("new_robot_controller"),
-        "launch",
-        "simple_controller.launch.py")
+    # Add a simple delay before launching the joystick and controller
+    delayed_launch = TimerAction(
+        period=6.0,  # Adjust the delay as needed (5 seconds here)
+        actions=[
+            IncludeLaunchDescription(
+                os.path.join(get_package_share_directory("new_robot_controller"),
+                             "launch",
+                             "joystick_teleop.launch.py")
+            ),
+            IncludeLaunchDescription(
+                os.path.join(get_package_share_directory("new_robot_controller"),
+                             "launch",
+                             "simple_controller.launch.py")
+            )
+        ]
     )
 
-    joystick_teleop = IncludeLaunchDescription(
-        os.path.join(
-        get_package_share_directory("new_robot_controller"),
-        "launch",
-        "joystick_teleop.launch.py"
-        ),
-        condition=IfCondition(use_joystick)
-    )
+    return LaunchDescription([gazebo_sim, delayed_launch])
 
-    keyboard_node = Node(package="new_robot_controller",
-                            executable="keyboard_control",
-                            condition=UnlessCondition(use_joystick))
-
-    return LaunchDescription([gazebo_sim,controller,joystick_teleop, keyboard_node])
-
-    
